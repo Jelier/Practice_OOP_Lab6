e265b151b25b5a9f555cc08a16ceead0a3ee7065
Jelllier <Jelllier@mail.ru>
2018-02-10 14:08:35 +0700

    as

--- CustomInterface/CustomInterface/DerivedShapes.cs
+++ CustomInterface/CustomInterface/DerivedShapes.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace CustomInterface
 {
+    //Signed by Jelier
     class Circle : Shape
     {
         public Circle() { }
--- CustomInterface/CustomInterface/Program.cs
+++ CustomInterface/CustomInterface/Program.cs
@@ -8,10 +8,28 @@ namespace CustomInterface
 {
     class Program
     {
+        //Signed by Jelier
         static void Main(string[] args)
         {
             Hexagon hex = new Hexagon();
             Console.WriteLine("Points: {0}", hex.Points);
+            Circle c = new CustomInterface.Circle("Lisa");
+            IPointy itfPt = null;
+            try
+            {
+                itfPt = (IPointy)c;
+                Console.WriteLine(itfPt.Points);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Hexagon hex2 = new Hexagon("Peter");
+            IPointy itfPt2 = hex2 as IPointy;
+            if (itfPt2 != null)
+                Console.WriteLine("Points: {0}", itfPt2.Points);
+            else
+                Console.WriteLine("OOPS! Not pointy...");
             Console.ReadLine();
         }
     }
