e1392e17ebb17abd6597e19adee4415f680b9e00
Jelllier <Jelllier@mail.ru>
2018-02-10 13:30:12 +0700

    first commit

---
+++ CustomInterface/CustomInterface/DerivedShapes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    class Circle : Shape
+    {
+        public Circle() { }
+        public Circle(string name) : base(name) { }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0}, the Circle", PetName);
+        }
+    }
+
+    class Hexagon : Shape, IPointy
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0}, the Hexagon", PetName);
+        }
+
+        public byte Points
+        {
+            get { return 6; }
+        }
+    }
+
+    class Triangle : Shape, IPointy
+    {
+        public Triangle() { }
+        public Triangle(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0}, the Triangle", PetName);
+        }
+
+        public byte Points
+        {
+            get { return 3; }
+        }
+    }
+}
