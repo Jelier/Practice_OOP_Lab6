c8ff1f31eec6e6d3f5fb6bf61eef54de9916be22
Jelllier <Jelllier@mail.ru>
2018-02-10 15:35:02 +0700

    Draw All

---
+++ InterfaceHierarchy/InterfaceHierarchy/BitmapImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+        {
+            Console.WriteLine("Drawing in a Box...");
+        }
+
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing upside down!");
+        }
+    }
+}
+
---
+++ InterfaceHierarchy/InterfaceHierarchy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        //Signed by Jelier
+        static void Main(string[] args)
+        {
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpsideDown();
+
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+            Console.ReadLine();
+        }
+    }
+}
